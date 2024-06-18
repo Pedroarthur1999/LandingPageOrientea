e698a55d0179695838104d0f2c68121f2a49b948
Pedro Arthur <pedroarthur1999@gmail.com>
2024-06-17 22:04:46 -0300

    feat: Add scroll review

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ $(document).ready(function() {
 
 
 
+    ScrollReveal().reveal('#home', {
+      origin: 'bottom',
+      distance: '250px',
+      duration: 1000,
+      delay: 500,
+      easing: 'ease',
+      reset: false
+    });
+
+    ScrollReveal().reveal('#sobre', {
+      origin: 'right',
+      distance: '250px',
+      duration: 1000,
+      delay: 500,
+      easing: 'ease',
+      reset: false
+    });
+
+
+    ScrollReveal().reveal('#services', {
+      origin: 'left',
+      distance: '250px',
+      duration: 1000,
+      easing: 'ease',
+      reset: false
+    });
 
 
 
