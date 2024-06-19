8d297fd5c5b5c00d0205886bb0eb7e28b1751adb
Pedro Arthur <pedroarthur1999@gmail.com>
2024-06-19 14:34:56 -0300

    feat: Add library scroll review

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-
-
-
-$(document).ready(function() {
-    $(".hamburguer").click(function() {
-        console.log('clicou');
-        $(this).toggleClass("show-menu");
-        $('.sidebar').toggleClass("toggled")
-    });
+$(document).ready(function () {
+  $(".hamburguer").click(function () {
+    console.log("clicou");
+    $(this).toggleClass("show-menu");
+    $(".sidebar").toggleClass("toggled");
+  });
 });
 
+ScrollReveal().reveal("#home", {
+  origin: "right",
+  distance: "250px",
+  duration: 1000,
+  delay: 500,
+  easing: "ease",
+  reset: false,
+});
 
+ScrollReveal().reveal("#sobre", {
+  origin: "bottom",
+  distance: "250px",
+  duration: 1000,
+  delay: 200,
+  easing: "ease",
+  reset: false,
+});
 
-    ScrollReveal().reveal('#home', {
-      origin: 'bottom',
-      distance: '250px',
-      duration: 1000,
-      delay: 500,
-      easing: 'ease',
-      reset: false
-    });
-
-    ScrollReveal().reveal('#sobre', {
-      origin: 'right',
-      distance: '250px',
-      duration: 1000,
-      delay: 500,
-      easing: 'ease',
-      reset: false
-    });
-
-
-    ScrollReveal().reveal('#services', {
-      origin: 'left',
-      distance: '250px',
-      duration: 1000,
-      easing: 'ease',
-      reset: false
-    });
-
-
-
-
+ScrollReveal().reveal("#services", {
+  origin: "bottom",
+  distance: "250px",
+  duration: 1500,
+  easing: "ease",
+  delay: 200,
+  reset: false,
+});
+ScrollReveal().reveal("#app", {
+  origin: "bottom",
+  distance: "250px",
+  duration: 1500,
+  easing: "ease",
+  delay: 200,
+  reset: false,
+});
